c2713e3a11eca70a1b43eeba9483f7adeeabbf2d
Stefano Menegon <ste.menegon@gmail.com>
2021-08-02 16:13:33 +0200

    added imagefield to location

--- measurements/models.py
+++ measurements/models.py
@@ -68,6 +68,7 @@ class Location(models.Model):
     label = models.CharField(max_length=150)
     geo = models.PointField(srid=4326, null=True)
     elevation = models.IntegerField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     objects = LocationManager()
     extra = PostgresManager()
